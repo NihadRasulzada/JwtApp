cb947dc5cb0d424441113ee353e17ffc1fbf751a
NihadRasulzada <nihadrs2005@gamil.com>
2024-05-29 00:15:28 +0400

    AddRepository

---
+++ JwtApp.Back/Persistance/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JwtApp.Back.Core.Domain;
+using JwtApp.Back.Persistance.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtApp.Back.Persistance.Context
+{
+    public class AppDbContext : DbContext
+    {
+        #region AppDbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        #endregion
+
+        #region OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+        }
+        #endregion
+
+        public DbSet<Product> Products => this.Set<Product>();
+        public DbSet<Category> Categories => this.Set<Category>();
+        public DbSet<AppUser> AppUsers => this.Set<AppUser>();
+        public DbSet<AppRole> AppRoles => this.Set<AppRole>();
+    }
+}
